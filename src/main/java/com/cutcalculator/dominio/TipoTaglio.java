81039bbe87b81a553b2023993a7e097596ab0d75
smacchia-polimi <simone1.macchia@mail.polimi.it>
2026-08-04 16:21:05 +0200

    aggiunti ordini e distinte per i pezzi

--- src/main/java/com/cutcalculator/dominio/TipoTaglio.java
+++ src/main/java/com/cutcalculator/dominio/TipoTaglio.java
@@ -3,7 +3,7 @@ package com.cutcalculator.dominio;
 /**
  * Combinazione dei due tagli alle estremità di un pezzo, tenuta in un solo valore.
  * I tagli sono quasi sempre a 45° o 90°, quindi le combinazioni utili sono poche.
- * Il caso misto è {@link #TAGLIO_90_45}.
+ * Il caso misto è {@link #TAGLIO_90_45}
  */
 public enum TipoTaglio {
     TAGLIO_45_45,
